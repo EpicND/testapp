16d7d37712c4fb2ca80ce15550123a957db38aa8
DhairyaG23 <dhairyag23@gmail.com>
2020-05-13 22:30:11 -0500

    Update main.dart

--- frc_scouting_test/lib/main.dart
+++ frc_scouting_test/lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
+
+  
+
+
   runApp(
     MaterialApp(
-      home: Scaffold(
+      home: Home(),
+    ),
+  );
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text('FRC Scout'),
           centerTitle: true,
+          backgroundColor: Colors.black87,
         ),
-      ),
-    ),
-  );
+      );
+  }
 }
