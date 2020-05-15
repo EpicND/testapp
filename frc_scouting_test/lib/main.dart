f84b06c1f4d16f3e5f570d745b69bb0b2eb0a4be
DhairyaG23 <dhairyag23@gmail.com>
2020-05-14 20:07:14 -0500

    epic

--- frc_scouting_test/lib/main.dart
+++ frc_scouting_test/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:frc_scouting_test/pages/home.dart';
+import 'package:frc_scouting_test/pages/choose_event.dart';
 
 void main() {
   runApp(
     MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => Home(),
+        '/': (context) => ChooseLocation(),
       },
     ),
   );
--- frc_scouting_test/lib/pages/choose_event.dart
+++ frc_scouting_test/lib/pages/choose_event.dart
@@ -1,16 +1,86 @@
 import 'package:flutter/material.dart';
 
-
 class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  
+  TextEditingController editingController = TextEditingController();
+  final duplicateItems = List<String>.generate(10000, (index) => "Item $index");
+  var items = List<String>();
+
+
+  // @override
+  void filterSearch(String query){
+    List<String> dummySearchList = List<String>();
+    dummySearchList.addAll(duplicateItems);
+    if(query.isNotEmpty){
+      List<String> dummyListData = List<String>();
+      dummySearchList.forEach((element) {
+        if(element.contains(query)){
+          dummyListData.add(element);
+        }
+      });
+      setState((){
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+      } else {
+        setState((){
+          items.clear();
+          items.addAll(duplicateItems);
+        });
+      }
+    }
+  
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    items.addAll(duplicateItems);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('Teams')),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(0.8),
+              child: TextField(
+                onChanged: (value) {filterSearch(value);},
+                controller: editingController,
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  )),
+                ),
+              ),
+            ),
+            Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('${items[index]}'),
+                );
+              },
+            ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
