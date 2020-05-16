9723ff8db2d16e9699ca8c787253b3922a5319f8
DhairyaG23 <dhairyag23@gmail.com>
2020-05-16 17:56:36 -0500

    Update cupertino_nav_bar.dart

--- frc_scouting_test/lib/widgets/cupertino_nav_bar.dart
+++ frc_scouting_test/lib/widgets/cupertino_nav_bar.dart
@@ -12,12 +12,47 @@ class CuptertinoNavBar extends StatelessWidget {
           ),
           BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.group),
+            title: Text('Teams')
           ),
 
         ],
       ),
-      tabBuilder: ,
-    ),
+     tabBuilder: (BuildContext context, int index) {
+    return CupertinoTabView(
+      builder: (BuildContext context) {
+        return CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(
+            middle: Text('Page 1 of tab $index'),
+          ),
+          child: Center(
+            child: CupertinoButton(
+              child: const Text('Next page'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  CupertinoPageRoute<void>(
+                    builder: (BuildContext context) {
+                      return CupertinoPageScaffold(
+                        navigationBar: CupertinoNavigationBar(
+                          middle: Text('Page 2 of tab $index'),
+                        ),
+                        child: Center(
+                          child: CupertinoButton(
+                            child: const Text('Back'),
+                            onPressed: () { Navigator.of(context).pop(); },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  },
+    );
     
   }
 }
