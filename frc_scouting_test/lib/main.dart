aa63d54e24c13d49ece34a05f7a45f9ad40f2dda
DhairyaG23 <dhairyag23@gmail.com>
2020-05-16 19:33:44 -0500

    add file read write code

--- frc_scouting_test/lib/main.dart
+++ frc_scouting_test/lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:frc_scouting_test/pages/home.dart';
 import 'package:frc_scouting_test/pages/choose_event.dart';
 import 'package:frc_scouting_test/widgets/cupertino_nav_bar.dart';
+import 'package:frc_scouting_test/widgets/cupertino_app.dart';
 
 void main() {
+  
   runApp(
     MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => CupertinoNavBar(),
+        '/': (context) => Home(),
         '/choose_event': (context) => ChooseLocation(),
       },
     ),
