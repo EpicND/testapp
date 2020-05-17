d6f76dcc0b0a8af05732b0ab518fd20c72b8283d
ccans <55026208+ccans@users.noreply.github.com>
2020-05-17 00:38:26 -0500

    okay then

--- frc_scouting_test/lib/main.dart
+++ frc_scouting_test/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frc_scouting_test/pages/home.dart';
 import 'package:frc_scouting_test/pages/choose_event.dart';
+import 'package:frc_scouting_test/widgets/android.dart';
 import 'package:frc_scouting_test/widgets/cupertino_nav_bar.dart';
 
 void main() {
@@ -8,7 +9,7 @@ void main() {
     MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => CupertinoNavBar(),
+        '/': (context) => Android(),
         '/choose_event': (context) => ChooseLocation(),
       },
     ),
